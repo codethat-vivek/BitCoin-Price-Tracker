180398bb23a5eeb8573dc7ec9b68103092080b1c
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-04-13 14:42:04 +0530

    Create bitcoin.py

---
+++ bitcoin.py
@@ -0,0 +1,8 @@
+# importing different libraries
+
+import tkinter as tk # python GUI library
+import requests # run 'pip install requests' in cmd
+
+# Function for tracking bitCoin price
+def trackBitCoin():
+  url = ""
